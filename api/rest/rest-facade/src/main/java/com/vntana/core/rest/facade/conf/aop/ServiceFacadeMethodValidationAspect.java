42f1627ae9547168d0c1def99cec26fcc1168e32
Arthur Asatryan <biacoder@gmail.com>
2019-10-09 18:40:08 +0400

    [VP20-19] Boy scouting

--- api/rest/rest-facade/src/main/java/com/vntana/core/rest/facade/conf/aop/ServiceFacadeMethodValidationAspect.java
+++ api/rest/rest-facade/src/main/java/com/vntana/core/rest/facade/conf/aop/ServiceFacadeMethodValidationAspect.java
@@ -24,7 +24,8 @@ import java.util.List;
 public class ServiceFacadeMethodValidationAspect {
 
     @Around("execution(public * com.vntana.core.rest.facade.*.impl..* (..)) " +
-            "&& args(validatableRequest,..)")
+            "&& args(validatableRequest,..)"
+    )
     public Object around(final ProceedingJoinPoint point, final ValidatableRequest<?> validatableRequest) throws Throwable {
         Assert.notNull(validatableRequest, "The validatable request should not be null");
         final Method method = ((MethodSignature) point.getSignature()).getMethod();
