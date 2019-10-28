c93ed56f3debef9ee658bf7f2a2933db4e501f75
gevorgyanarman <armangevorg.post@gmail.com>
2019-10-28 14:54:43 +0400

    created TemplateEmail domain with getByType service method

---
+++ domain/src/main/java/com/vntana/core/domain/template/email/TemplateEmailType.java
@@ -0,0 +1,10 @@
+package com.vntana.core.domain.template.email;
+
+/**
+ * Created by Arman Gevorgyan.
+ * Date: 10/28/19
+ * Time: 10:30 AM
+ */
+public enum TemplateEmailType {
+    FORGET_PASSWORD
+}
---
+++ service/service-integration-test/src/test/kotlin/com/vntana/core/service/template/email/test/TemplateEmailServiceGetByTypeIntegrationTest.kt
@@ -0,0 +1,22 @@
+package com.vntana.core.service.template.email.test
+
+import com.vntana.core.domain.template.email.TemplateEmailType
+import com.vntana.core.service.template.email.AbstractTemplateEmailServiceIntegrationTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+/**
+ * Created by Arman Gevorgyan.
+ * Date: 10/28/19
+ * Time: 1:30 PM
+ */
+class TemplateEmailServiceGetByTypeIntegrationTest : AbstractTemplateEmailServiceIntegrationTest() {
+
+    @Test
+    fun `test`() {
+        templateEmailService.getByType(TemplateEmailType.FORGET_PASSWORD).let {
+            assertThat(it.type).isEqualTo(TemplateEmailType.FORGET_PASSWORD)
+            assertThat(it.templateName).isNotNull()
+        }
+    }
+}
