03e4ee1390242bb56503c9997b039e71473d62b9
gevorgyanarman <armangevorg.post@gmail.com>
2019-12-26 17:51:05 +0400

    minor change

--- api/rest/rest-model/src/main/java/com/vntana/core/model/organization/response/get/GetOrganizationByUuidResponseModel.java
+++ api/rest/rest-model/src/main/java/com/vntana/core/model/organization/response/get/GetOrganizationByUuidResponseModel.java
@@ -24,6 +24,7 @@ public class GetOrganizationByUuidResponseModel extends AbstractUuidAwareRespons
     @JsonProperty("imageId")
     private String imageId;
 
+    @JsonProperty("created")
     private LocalDateTime created;
 
     public GetOrganizationByUuidResponseModel() {
