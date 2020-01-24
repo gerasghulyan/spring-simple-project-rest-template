04873a814e2b4e523f619f0a21d5c7b040b0b250
gevorgyanarman <armangevorg.post@gmail.com>
2020-01-24 10:42:13 +0400

    made AuthResourceClient Feign client primary=false

--- api/rest/rest-client/src/main/java/com/vntana/core/rest/client/auth/AuthResourceClient.java
+++ api/rest/rest-client/src/main/java/com/vntana/core/rest/client/auth/AuthResourceClient.java
@@ -1,8 +1,8 @@
 package com.vntana.core.rest.client.auth;
 
 import com.vntana.core.model.security.request.FindUserByUuidAndOrganizationRequest;
-import com.vntana.core.model.security.response.SecureFindUserByUuidAndOrganizationResponse;
 import com.vntana.core.model.security.response.SecureFindUserByEmailResponse;
+import com.vntana.core.model.security.response.SecureFindUserByUuidAndOrganizationResponse;
 import com.vntana.core.model.user.request.FindUserByEmailRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
  * Date: 10/21/19
  * Time: 3:48 PM
  */
-@FeignClient(name = "coreAuthResourceClient", path = "auth", url = "${ms.core.url}")
+@FeignClient(name = "coreAuthResourceClient", path = "auth", url = "${ms.core.url}", primary = false)
 public interface AuthResourceClient {
 
     @PostMapping(path = "/by-email")
