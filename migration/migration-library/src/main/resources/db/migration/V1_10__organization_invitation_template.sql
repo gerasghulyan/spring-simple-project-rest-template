2526923b7bfd05f26d9e0c8680208bbbe87ca1b7
a.gevorgyan <arman.gevorgyan@sfl.am>
2020-03-30 14:13:44 +0400

    fixed migration

--- migration/migration-library/src/main/resources/db/migration/V1_10__organization_invitation_template.sql
+++ migration/migration-library/src/main/resources/db/migration/V1_10__organization_invitation_template.sql
@@ -1,4 +1,4 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 insert into template_email (id, created, removed, updated, uuid, template_name, type)
-VALUES (nextval('template_email_seq'), now(), null, now(), uuid_generate_v4(), 'template_organization_invitation', 'ORGANIZATION_INVITATION');
+VALUES (nextval('template_email_seq'), now(), null, now(), public.uuid_generate_v4(), 'template_organization_invitation', 'ORGANIZATION_INVITATION');
