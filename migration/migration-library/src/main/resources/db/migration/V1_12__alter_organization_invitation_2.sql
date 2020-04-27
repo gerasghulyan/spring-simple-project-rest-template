24539bf03ce8ea38fc8a5107f0afbf42ffa2c64e
gerasghulyan <geras.ghulyan@sflpro.com>
2020-04-27 18:04:59 +0400

    boy scouting

--- migration/migration-library/src/main/resources/db/migration/V1_12__alter_organization_invitation_2.sql
+++ migration/migration-library/src/main/resources/db/migration/V1_12__alter_organization_invitation_2.sql
@@ -2,7 +2,7 @@ alter table organization
     add column invitation_organization_id bigint;
 alter table organization
     add constraint fk_organization_invitation_organization_id foreign key (invitation_organization_id) references invitation_organization,
-    add constraint uk_organization_invitation_organization_id unique (uuid);
+    add constraint uk_organization_invitation_organization_id unique (invitation_organization_id);
 
 alter table invitation_organization
     drop constraint fk_settings_whitelist_ip_organization_id;
