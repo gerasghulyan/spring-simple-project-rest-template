f6c4c1cd564437bb1baa16145965038d77d902d6
a.gevorgyan <arman.gevorgyan@sfl.am>
2020-05-04 12:49:54 +0400

    removed notification configuration form notification module, added beans for tests and normal run

--- api/rest/rest-resource-integration-test/src/test/kotlin/com/vntana/core/rest/resource/conf/WebIntegrationTestConfiguration.kt
+++ api/rest/rest-resource-integration-test/src/test/kotlin/com/vntana/core/rest/resource/conf/WebIntegrationTestConfiguration.kt
@@ -3,8 +3,12 @@ package com.vntana.core.rest.resource.conf
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Lazy
+import org.springframework.context.annotation.Primary
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import java.util.concurrent.Executor
 
 /**
  * Created by Arthur Asatryan.
@@ -16,4 +20,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 @EnableJpaRepositories("com.vntana.core")
 @EntityScan("com.vntana.core")
 @TestConfiguration
-class WebIntegrationTestConfiguration
+class WebIntegrationTestConfiguration {
+
+    @Primary
+    @Lazy(false)
+    @Bean(name = ["notificationSenderExecutor"])
+    fun executor(): Executor = Executor { it.run() }
+
+}
